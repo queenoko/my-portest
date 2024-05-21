81883d60a258ac8071603107fd903a445a7ada68
queenoko <quimaudongwo@gmail.com>
2024-05-21 12:02:16 +0100

    portest

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $blogs = [
+            [
+                'title' => 'title one',
+                'body' => 'this is body text',
+                'status' => 0
+            ],
+            [
+                'title' => 'title two',
+                'body' => 'this is body text',
+                'status' => 0
+            ],
+            [
+                'title' => 'title three',
+                'body' => 'this is body text',
+                'status' => 0
+            ],
+            [
+                'title' => 'title four',
+                'body' => 'this is body text',
+                'status' => 0
+            ]
+            ];
+            return view('home', compact('blogs'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+{{-- main content begins here --}}
+@section('content')
+<main role="main" class="container">
+  <h1 class="mt-5 text-danger">Home</h1>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ratione quaerat vero a, ullam reiciendis 
+  earum distinctio nihil exercitationem quidem neque odit aliquid quasi 
+  esse, repudiandae, adipisci non placeat.
+
+  <div class="row mt-5">
+
+  </div>
+</main>
+@endsection
+
+          
