cfddcbd19dfee8e8d4c48bdede296aa486184512
queenoko <quimaudongwo@gmail.com>
2024-08-08 07:52:01 +0100

    CSRF1

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,9 @@ Route::get('/', function () {
 
 Route::get('/home', HomeController::class);
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+Route::post('/login', [LoginController::class, 'handleLogin'])->name('login.submit');
 
 // CSRF Token
 
