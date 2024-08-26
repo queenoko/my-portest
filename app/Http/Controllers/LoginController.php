36686fdbb70533664711c5433f8b627e6b7c8f15
queenoko <quimaudongwo@gmail.com>
2024-08-26 04:15:42 +0100

    form validation request classes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -11,18 +12,8 @@ class LoginController extends Controller
         return view('login');
     }
 
-    public function handleLogin(Request $request)
+    public function handleLogin(LoginRequest $request)
     {
-        $request->validate([
-            'name' => ['required', 'alpha', 'min:6', 'max:10'],
-            'email' => ['required', 'email'],
-            'password' => 'required'
-        ], [
-            'name.required' => 'The user name field is required!',
-            'name.alpha' => 'User name should contain only letters',
-            'email.email' => 'User email must be correct',
-            'name.min' => 'Must have six letters'
-        ]);
         return $request;
     }
 }
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'alpha', 'min:6', 'max:10'],
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return[
+            'name.required' => 'The user name field is required!',
+            'name.alpha' => 'User name should contain only letters',
+            'email.email' => 'User email must be correct',
+            'name.min' => 'Must have six letters'
+        ];
+    }
+}
