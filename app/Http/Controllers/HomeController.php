5b9e8b08ad59364c96eb31c32ed4f049348264b3
queenoko <quimaudongwo@gmail.com>
2024-08-28 15:33:04 +0100

    add column to DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,29 +12,8 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $blogs = [
-            [
-                'title' => 'title one',
-                'body' => 'this is body text',
-                'status' => 0
-            ],
-            [
-                'title' => 'title two',
-                'body' => 'this is body text',
-                'status' => 0
-            ],
-            [
-                'title' => 'title three',
-                'body' => 'this is body text',
-                'status' => 1
-            ],
-            [
-                'title' => 'title four',
-                'body' => 'this is body text',
-                'status' => 0
-            ]
-            ];
-            return view('home', compact('blogs'));
+        return DB::table('posts')->pluck('title', 'id');
+        return view('home', compact('blogs'));
     }
     
 }
