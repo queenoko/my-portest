842d9dc780716a3cc1cc878663fc044c5d433c83
queenoko <quimaudongwo@gmail.com>
2024-08-28 16:17:17 +0100

    delete from database1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        DB::table('posts')->delete(53);
+        DB::table('posts')->Where('id', 52)->delete();
 
         dd('success');
         
