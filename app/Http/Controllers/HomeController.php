de99f6c9ad5b03b983bc45724fa9f39dadcea9b2
queenoko <quimaudongwo@gmail.com>
2025-02-07 13:58:05 +0100

    add image controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,12 +15,7 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //Post::withTrashed()->find(2)->forceDelete();
-        //Post::withTrashed()->find(4)->restore();
-        //return Post::onlyTrashed()->get();
-        //Post::where('id', 2)->delete();
-        //return Post::all();
-        //dd('success');
+        return view('home');
     }
     
 }
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
           bottom: 0;">          
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-              © 2024 Copyright:
+              © 2025 Copyright:
               <a class="text-dark" href="">QuimaTechGlobal</a>
             </div>
             <!-- Copyright -->
