a1e8dd3ce8b087dc8238ae476414d8491e07a286
queenoko <quimaudongwo@gmail.com>
2025-02-26 15:45:56 +0100

    validate and image size

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,7 +9,8 @@ class ImageController extends Controller
     public function handleImage(Request $request)
     {
         $request->validate([
-            'image' => 'required',
+            'image' => ['required', 'min:100', 'max:500', 'mimes:png,jpg,gif'] //500kb
+
         ]);
 
         $request->image->storeAs('/images', 'new_image.jpg');
